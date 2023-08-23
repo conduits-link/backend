1530e59d236591d2de91ac338ac8a14134562d8a
jlh <11036537+jhels@users.noreply.github.com>
2023-08-23 10:20:53 +0100

    Added editor file model

--- nw_backend/user_accounts/models.py
+++ nw_backend/user_accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class EditorFile(models.Model):
+
+    title = models.CharField(max_length=50, help_text='Title of file')
+
+    # 25000 chars ≈ 5000 words. Temporary length limit.
+    file_text = models.CharField(max_length=25000, help_text='Text stored in file')
+
+    date_created = models.DateTimeField()
+
+    class Meta:
+        # Order files from most recent to oldest.
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+    
+
+class User(AbstractUser):
+    pass
